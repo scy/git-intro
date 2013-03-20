4439ba053b31eb91b77d30e027ef1407938b346b
Tim Weber <scy@scy.name>
2013-03-20 18:20:20 +0100

    Trim <pre><code> contents

--- prepare-dom.js
+++ prepare-dom.js
@@ -1,4 +1,8 @@
 (function ($) {
 	'use strict';
 	$('section').addClass('slide');
+	$('pre > code').each(function (idx, code) {
+		var $code = $(code);
+		$code.html($.trim($code.html()));
+	});
 })(jQuery);
