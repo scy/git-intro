327310d038141ea721f1640599bf7cb091313f5f
Tim Weber <scy@scy.name>
2013-03-21 14:01:22 +0100

    Add animate.css and animate only visible slides

--- git-intro.js
+++ git-intro.js
@@ -3,4 +3,10 @@
 	$.deck('.slide');
 	hljs.tabReplace = '  ';
 	hljs.initHighlightingOnLoad();
+	$(document).bind('deck.change', function (ev, from, to) {
+		var $from = $.deck('getSlide', from);
+		var $to = $.deck('getSlide', to);
+		$from.find('.repeat-animation').removeClass('animated');
+		$to.find('.repeat-animation').addClass('animated');
+	});
 })(jQuery);
